2c77c285f56c009ccc0df162ec73fef98924da08
Baby_Moico <babyta.rpp@gmail.com>
2015-05-25 22:01:48 -0400

    [feature] create route for club Description ======== Add club-model Add club-route

---
+++ models/club-model.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Baby_Moico on 25/05/2015.
+ */
+"use strict";
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var ClubSchema = new Schema({
+    'name':{
+        'type': String,
+        'default': ""
+    },
+    'foundation':{
+        'type':Date,
+        'default':""
+    },
+    'delegate':{
+        'type':String,
+        'default':""
+    }
+});
+
+var ClubModel = mongoose.model('Club', ClubSchema);
+module.exports = ClubModel;
+
+
+
+
--- routes/api-v1.0/index.js
+++ routes/api-v1.0/index.js
@@ -1,9 +1,11 @@
 var UserRoutes = require('./user-route');
 var TeamRoutes = require('./team-route');
 var PlayerRoutes = require('./player-route');
+var ClubRoutes = require('./club-route');
 
 module.exports = function(app){
     UserRoutes(app);
     TeamRoutes(app);
     PlayerRoutes(app);
+    ClubRoutes(app);
 };

91742579645e174bb665def4e76824d1fedb3801
Adritay <adritaberrios@gmail.com>
2015-05-29 00:23:20 -0400

    [feature] Increase fields to the player-model. Description ======== Added fields to the player-model. Added sub-documents to the player-model.

--- models/player-model.js
+++ models/player-model.js
@@ -2,6 +2,40 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+var CvSchema = new Schema({
+    "year": {
+        "type":Number,
+        "default":""
+    },
+    "club_name":{
+        "type":String,
+        "default":""
+    },
+    "asociation":{
+        "type":String,
+        "default":""
+    },
+    "category":{
+        "type":String,
+        "default":""
+    },
+    "city":{
+        "type":String,
+        "default":""
+    },
+    "type":{
+        'type': String,
+        'index': true,
+        'required': true,
+        'default': "Club",
+        'enum': [
+            "Club",
+            "Seleccion",
+            "Seleccion Nacional"
+        ]
+    }
+});
+
 var PlayerSchema = new Schema({
     'name': {
         'type': String,
@@ -13,13 +47,33 @@ var PlayerSchema = new Schema({
         'default': "",
         'required': true
     },
+    'city': {
+        'type': String,
+        'default': "",
+        'required': true
+    },
+    'nationality': {
+        'type': String,
+        'default': "",
+        'required': true
+    },
     'ci': {
         'type': String,
         'default': "",
         'required': true
     },
     'date_of_birth': {
-        'type': Date,
+        'type': Number,
+        'default': "",
+        'required': true
+    },
+    'month_of_birth': {
+        'type': String,
+        'default': "",
+        'required': true
+    },
+    'year_of_birth': {
+        'type': Number,
         'default': "",
         'required': true
     },
@@ -31,10 +85,52 @@ var PlayerSchema = new Schema({
         'type': String,
         'default': ""
     },
-    'club':{
+    'zone': {
+        'type': String,
+        'default': ""
+    },
+    'postcode': {
+        'type': String,
+        'default': ""
+    },
+    'ofice_birth_cert':{
         'type': String,
         'default': "",
         'required': true
+    },
+    'book_birth_cert':{
+        'type': String,
+        'default': "",
+        'required': true
+    },
+    'departure_birth_cert':{
+        'type': String,
+        'default': "",
+        'required': true
+    },
+    'departure_date_birth_cert':{
+        'type': Date,
+        'default': "",
+        'required': true
+    },
+    'branch': {
+        'type': String,
+        'index': true,
+        'required': true,
+        'default': "---------",
+        'enum': [
+            "---------",
+            "Femenino",
+            "Masculino"
+        ]
+    },
+    'club':{
+        'type':String,
+        "required":true
+    },
+    'cv_player':{
+        "type":[CvSchema],
+        "default":""
     }
 });
 

5f9be1175ba64f10a3ed490d2d7e71fb2605e2a1
RuthP <babyta.rpp@gmail.com>
2015-07-18 21:49:10 -0400

    [feature] Calculate age and validate fields Description ======== Add validate fields in edit and creation of medical record. Add age in the list of players in medical record.

--- public/js/controllers/listMedicalRecord-controller.js
+++ public/js/controllers/listMedicalRecord-controller.js
@@ -5,12 +5,21 @@ advcApp.controller('listMedicalRecordCtrl', ['$scope', '$routeParams',
         $scope.Medicals = {};
         $scope.newMedicalRecord = {};
         $scope.Players = {};
+        $scope.format = 'dd/MM/yyyy';
 
         $scope.formCreateMedical = function () {
             $scope.newMedicalRecord = {};
             $scope.createMode = true;
             $scope.editMode = false;
         };
+
+        var restartValidationFields = function(){
+            $scope.isWeightValid = true;
+            $scope.isHeightValid = true;
+            $scope.isHomeValid = true;
+        };
+
+        restartValidationFields();
 
         listPlayersSrv.get({},
             function(result){
@@ -20,6 +29,7 @@ advcApp.controller('listMedicalRecordCtrl', ['$scope', '$routeParams',
                     result.data[index].fullName = fullName;
                 }
                 $scope.Players = result.data;
+
             },
             function(error){
                 console.log(error);
@@ -36,36 +46,111 @@ advcApp.controller('listMedicalRecordCtrl', ['$scope', '$routeParams',
             }
         );
 
+        $scope.calculateAge = function(dateOfBirth){
+                var today = new Date();
+                var todayYear = today.getYear();
+                var todayMonth = today.getMonth();
+                var todayDate = today.getDate();
+
+
+                var birthDate = new Date(dateOfBirth);
+
+                var date = birthDate.getDate();
+                var month = birthDate.getMonth();
+                var year = birthDate.getYear();
+                var age = (todayYear + 1900) - year;
+
+                if ( todayMonth < (month - 1)){
+                    age--;
+                }
+                if (((month - 1) == todayMonth) && (todayDate < date)){
+                    age--;
+                }
+                if (age > 1900){
+                    age -= 1900;
+                }
+                return age;
+            };
+
+            $scope.validateFields = function () {
+            var weight = $scope.newMedicalRecord.weight;
+            var height = $scope.newMedicalRecord.height;
+            var home= $scope.newMedicalRecord.home;
+            var weightRegEx = /^-?[0-9]+([\.][0-9]*)?$/;
+            var heightRegEx = /^-?[0-9]+([\.][0-9]*)?$/;
+            var homeRegEx = /[A-Za-z0-9_]{3,20}/;
+
+            if (!weight || !weight.trim()) {
+                $.noty.consumeAlert({layout: 'topCenter',
+                    type: 'warning', dismissQueue: true ,
+                    timeout:2000 });
+                alert('Ingrese el peso del jugador.');
+                $.noty.stopConsumeAlert();
+                return false;
+            }else if (!weightRegEx.test(weight)) {
+                $scope.isWeightValid = false;
+                return false;
+
+            }else if (!height || !height.trim()) {
+                $.noty.consumeAlert({layout: 'topCenter',
+                    type: 'warning', dismissQueue: true ,
+                    timeout:2000 });
+                alert('Ingrese el altura del jugador.');
+                $.noty.stopConsumeAlert();
+                $scope.isWeightValid = true;
+                return false;
+            }else if (!heightRegEx.test(height)) {
+                $scope.isWeightValid = true;
+                $scope.isHeightValid = false;
+                return false;
+
+            }else if (!home || !home.trim()) {
+                $.noty.consumeAlert({layout: 'topCenter',
+                    type: 'warning', dismissQueue: true ,
+                    timeout:2000 });
+                alert('Ingrese la direccion del jugador.');
+                $.noty.stopConsumeAlert();
+                $scope.isHeightValid = true;
+                return false;
+            }else if (!homeRegEx.test(home)) {
+                $scope.isHeightValid = true;
+                $scope.isHomeValid = false;
+                return false;
+            }
+            return true;
+        };
+
         $scope.createMedicalRecord= function () {
             var newMedical = {
                 player: $scope.newMedicalRecord.player ?
                     $scope.newMedicalRecord.player._id : null,
-                age: $scope.newMedicalRecord.age,
                 weight: $scope.newMedicalRecord.weight,
                 height: $scope.newMedicalRecord.height,
                 home: $scope.newMedicalRecord.home,
                 allergies: $scope.newMedicalRecord.allergies,
                 operations: $scope.newMedicalRecord.operations
             };
-            listMedicalRecordSrv.save({medicalRecord: newMedical},
-                function (data) {
-                    var playerId = data.player;
-                    var index = 0;
-                    for(index; index < $scope.Players.length; index++){
-                        if($scope.Players[index]._id === playerId){
-                            data.player = $scope.Players[index];
+            if($scope.validateFields()){
+                listMedicalRecordSrv.save({medicalRecord: newMedical},
+                    function (data) {
+                        var playerId = data.player;
+                        var index = 0;
+                        for(index; index < $scope.Players.length; index++){
+                            if($scope.Players[index]._id === playerId){
+                                data.player = $scope.Players[index];
+                            }
                         }
+                        $scope.Medicals.push(data);
+                        $('#create-medical').modal('hide'); //hide modal
+                        $('body').removeClass('modal-open');
+                        $('.modal-backdrop').remove();
+                        $scope.newMedicalRecord = {};
+                    },
+                    function(error){
+                        console.log(error);
                     }
-                    $scope.Medicals.push(data);
-                    $('#create-medical').modal('hide'); //hide modal
-                    $('body').removeClass('modal-open');
-                    $('.modal-backdrop').remove();
-                    $scope.newMedicalRecord = {};
-                },
-                function(error){
-                    console.log(error);
-                }
-            );
+                );
+            }
         };
 
         $scope.editMedicalRecord = function(){
@@ -74,7 +159,6 @@ advcApp.controller('listMedicalRecordCtrl', ['$scope', '$routeParams',
             var newDataMedical = {
                 player: $scope.newMedicalRecord.player ?
                     $scope.newMedicalRecord.player._id : null,
-                age: $scope.newMedicalRecord.age,
                 weight: $scope.newMedicalRecord.weight,
                 height: $scope.newMedicalRecord.height,
                 home: $scope.newMedicalRecord.home,
@@ -82,26 +166,28 @@ advcApp.controller('listMedicalRecordCtrl', ['$scope', '$routeParams',
                 operations: $scope.newMedicalRecord.operations
             };
 
-            listMedicalRecordSrv.update({medicalId: medicalId},
-                {medicalRecord: newDataMedical},
-                function (data) {
-                    var playerId = data.player;
-                    var index = 0;
-                    for(index; index < $scope.Players.length; index++){
-                        if($scope.Players[index]._id === playerId){
-                            data.player = $scope.Players[index];
+            if($scope.validateFields()) {
+                listMedicalRecordSrv.update({medicalId: medicalId},
+                    {medicalRecord: newDataMedical},
+                    function (data) {
+                        var playerId = data.player;
+                        var index = 0;
+                        for (index; index < $scope.Players.length; index++) {
+                            if ($scope.Players[index]._id === playerId) {
+                                data.player = $scope.Players[index];
+                            }
                         }
+                        $scope.Medicals[indexRecord] = data;
+                        $('#create-medical').modal('hide'); //hide modal
+                        $('body').removeClass('modal-open');
+                        $('.modal-backdrop').remove();
+                        $scope.newMedicalRecord = {};
+                    },
+                    function (error) {
+                        console.log(error);
                     }
-                    $scope.Medicals[indexRecord] = data;
-                    $('#create-medical').modal('hide'); //hide modal
-                    $('body').removeClass('modal-open');
-                    $('.modal-backdrop').remove();
-                    $scope.newMedicalRecord = {};
-                },
-                function(error){
-                    console.log(error);
-                }
-            );
+                );
+            }
         };
 
         $scope.deleteMedical = function(medical, index) {
@@ -119,6 +205,27 @@ advcApp.controller('listMedicalRecordCtrl', ['$scope', '$routeParams',
             }
         };
 
+        $scope.closeModal=function(){
+            $scope.newMedicalRecord = {};
+            restartValidationFields();
+        };
+        $scope.obtainFormatDate = function(date){
+            if(date) {
+                var foundDate = new Date(date);
+                return foundDate.toLocaleDateString();
+            }
+            return '';
+        };
+        $scope.toggleMin = function() {
+            $scope.minDate = $scope.minDate ? null : new Date();
+        };
+        $scope.toggleMin();
+
+        $scope.dateOptions = {
+            formatYear: 'yyyy',
+            startingDay: 1
+        };
+
         $scope.formEditMedical = function(medical, indexRecord){
             var index = 0;
             for(index; index < $scope.Players.length; index++){
@@ -129,7 +236,6 @@ advcApp.controller('listMedicalRecordCtrl', ['$scope', '$routeParams',
             $scope.editMode = true;
             $scope.newMedicalRecord = {
                 player: $scope.Players[index],
-                age: medical.age,
                 weight: medical.weight,
                 height: medical.height,
                 home: medical.home,
--- routes/api-v0.1/player-route.js
+++ routes/api-v0.1/player-route.js
@@ -1,5 +1,7 @@
 var route = require('./routes');
 var PlayerModel = require('../../models/player-model');
+var Q = require('q');
+
 var __bind = function(fn, me){
     return function(){
         return fn.apply(me, arguments);
@@ -13,6 +15,7 @@ var PlayerRoute = (function() {
         this.savePlayer = __bind(this.savePlayer, this);
         this.removePlayer = __bind(this.removePlayer, this);
         this.updatePlayer = __bind(this.updatePlayer, this);
+        this.calculateAge = __bind(this.calculateAge, this);
     }
 
     PlayerRoute.prototype.getPlayer = function(request, response){
@@ -27,7 +30,38 @@ var PlayerRoute = (function() {
         });
     };
 
+    PlayerRoute.prototype.calculateAge = function(data) {
+        var defer = Q.defer();
+        var today = new Date();
+        var todayYear = today.getYear();
+        var todayMonth = today.getMonth();
+        var todayDate = today.getDate();
+
+        for(var index in data){
+            var birthDate = new Date(data[index].date_of_birth);
+
+            var date = birthDate.getDate();
+            var month = birthDate.getMonth();
+            var year = birthDate.getYear();
+            var age = (todayYear + 1900) - year;
+
+            if ( todayMonth < (month - 1)){
+                age--;
+            }
+            if (((month - 1) == todayMonth) && (todayDate < date)){
+                age--;
+            }
+            if (age > 1900){
+                age -= 1900;
+            }
+            data[index].age = age;
+        }
+        defer.resolve(data);
+        return defer.promise;
+    };
+
     PlayerRoute.prototype.getPlayers = function(request, response){
+        var self = this;
         var filter = request.body;
         var query = PlayerModel.find(filter);
         query.exec(function(error, data) {

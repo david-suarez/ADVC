f353a3391b70dea5f2f59ac62de3864828fa1170
David <david.suarez87@gmail.com>
2015-08-06 17:22:42 -0400

    [config]  Config script to start service in openshift Description ======== Configure the conecction string to connect with database

--- app.js
+++ app.js
@@ -25,14 +25,18 @@ cloudinary.config({
 });
 
 //provide a sensible default for local development
-var db_name = 'advc-db';
-var mongodb_connection_string = 'mongodb://127.0.0.1:27017/' + db_name;
+var db_name = 'advc';
+var connection_string = 'mongodb://127.0.0.1:27017/' + db_name;
 //take advantage of openshift env vars when available:
-if(process.env.OPENSHIFT_MONGODB_DB_URL){
-    mongodb_connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + db_name;
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+    connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+        process.env.OPENSHIFT_APP_NAME;
 }
 
-mongoose.connect(mongodb_connection_string);
+mongoose.connect(connection_string);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
